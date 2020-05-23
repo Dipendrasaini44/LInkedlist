2a1da2367e9f838e4218b262a3a58310e4e53b0b
dipendrasaini <dipendrasaini44@gmail.com>
2020-05-23 08:57:20 +0530

    sat commit

--- list.py
+++ list.py
@@ -6,3 +6,37 @@ print(newlist)
 thislist = list(("apple", "banana", "cherry"))
 
 print(thislist)
+
+class Person:
+
+    def __init__(self,fname,lname):
+        self.fname= fname
+        self.lname= lname
+
+    def printg(self):
+        print(self.fname)
+
+
+class Student(Person):
+    def __init__(self, fname, lname,year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+
+    def print1(self):
+        print(self.graduationyear)
+
+
+e1= Student("Dipendra","saini",1997)
+
+e1.printg()
+e1.print1()
+
+
+# All these objects have a iter() method which is used to get an iterator:
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+# print(next(myit))
